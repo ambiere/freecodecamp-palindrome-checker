fe566c8cb84fe867a2b9c753b24379c9bdda3de3
zhid0399123 <zhid0399123@gmail.com>
2023-08-08 21:06:20 +0300

    feat(PalindromeHeader.jsx): added a header component to render the title/header section

---
+++ src/layout/PalindromeHeader.jsx
@@ -0,0 +1,10 @@
+import "../layout/palindromeHeader.css";
+
+function PalindromeHeader({ title }) {
+  return (
+    <div className="palindrome-header">
+      <h1>{title}</h1>
+    </div>
+  );
+}
+export default PalindromeHeader;
